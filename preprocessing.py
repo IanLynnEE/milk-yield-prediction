04a4238b73a58d75a3cdb7ebd5f58de433556067
IanLynnEE <66598373+IanLynnEE@users.noreply.github.com>
2022-06-05 04:50:54 +0800

    Update preprocessing.py 1. Add mean and std as feature. 2. Recommendation on validation set.

--- preprocessing.py
+++ preprocessing.py
@@ -7,6 +7,41 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
+from sklearn.svm import SVR
+
+def main():
+    train, test = get_all_features()
+    # Remove 64 data with volume == 0. 
+    train.drop(train.index[train.volume == 0], inplace=True)
+    # Realistic validation set based on number of new cows
+    valid = train.query('year == 2018 & month > 9').copy()
+    train.query('year < 2018 | month <= 9', inplace=True)
+    # Just like in real case, we don't know all the mean and std.
+    add_mean_std(train, valid)
+    # If we have mean and std, drop other features
+    drop_list = ['ID', 'ranch', 'serial', 'father', 'mother']
+    # Play with other features
+    drop_list.extend(['year', 'month'])
+    train.drop(drop_list, axis=1, inplace=True)
+    valid.drop(drop_list, axis=1, inplace=True)
+
+    x = train.drop('volume', axis=1).to_numpy()
+    xv = valid.drop('volume', axis=1).to_numpy()
+    y = train['volume'].to_numpy()
+    yv = valid['volume'].to_numpy()
+
+    regr = SVR()
+    regr.fit(x, y)
+    yp = regr.predict(xv)
+    print('RMSE =', RMSE(yv, yp))
+    plt.scatter(yv, yp, label='SVR')
+    plt.scatter(yv, valid['mean'], label='Mean')
+    plt.axis([0, 60, 0, 60])
+    plt.xlabel('True')
+    plt.ylabel('Predict')
+    plt.legend()
+    plt.show()
+    return
 
 
 def RMSE(yt, yp):
@@ -43,26 +78,31 @@ def get_all_features() -> tuple[pd.DataFrame, pd.DataFrame]:
     return train, test
 
 
-def plot_zero_mean(train: pd.DataFrame):
-    # Calculate the average volume of each cow.
-    shifted = np.empty(1)
-    spread = np.empty(1)
+def add_mean_std(train: pd.DataFrame, test: pd.DataFrame):
+    """Add the average volume of each cow. #TODO each deliveray
+
+    Args:
+        train (pd.DataFrame): No 0 or NaN in column "volume". 
+        test (pd.DataFrame): All NaN or 0 in column "volume".
+    """
     for i in train['serial'].unique():
         same_cow = train.loc[train.serial == i]
-        mean = same_cow['volume'].mean()
-        std = same_cow['volume'].std()
-        # Ignore the cows that have only one data in training set.
-        if same_cow.shape[0] != 0:
-            shifted = np.hstack((shifted, same_cow['volume'] - mean))
-            spread = np.hstack((spread, std))
-    sns.histplot(data=shifted)
-    plt.xlabel('Subtract (Mean of Each Cow) from (Milk Yield)')
-    plt.show()
-    sns.histplot(data=spread)
-    plt.xlabel('STD of Each Cow')
-    plt.show()
+        train.loc[train.serial==i, 'mean'] = same_cow['volume'].mean()
+        train.loc[train.serial==i, 'std'] = same_cow['volume'].std()
+    train['std'].fillna(value=0, inplace=True)
+    # TODO What should be the "mean" if there's only one reference?
+    for i in test['serial'].unique():
+        same_cow = train.loc[train.serial == i]
+        if same_cow.shape[0] > 0:
+            test.loc[test.serial==i, 'mean'] = same_cow['volume'].mean()
+            test.loc[test.serial==i, 'std'] = same_cow['volume'].std()
+    # Number of cows that have no reference.
+    # print(test.drop_duplicates(subset='serial')['mean'].isnull().sum())
+    # TODO What should be the "mean" if there's no reference?
+    test['mean'].fillna(value=train['volume'].mean(), inplace=True)
+    test['std'].fillna(value=0, inplace=True)
     return
 
 
 if __name__ == '__main__':
-    train, test = get_all_features()
+    main()
