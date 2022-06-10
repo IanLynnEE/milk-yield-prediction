7c72722a873f391882abe4b6f791fb199f092a4d
IanLynnEE <66598373+IanLynnEE@users.noreply.github.com>
2022-06-11 05:50:50 +0800

    Update preprocessing.py 1. Back to predict volume. 2. Use normal validation set. 3. Remove mean and std.

--- preprocessing.py
+++ preprocessing.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.model_selection import train_test_split
 
 from sklearn.svm import SVR
 from sklearn.ensemble import RandomForestRegressor
@@ -14,34 +15,29 @@ def main():
     report = get_all_features()
     # Remove 64 training data with volume == 0.
     report.drop(report.index[report.volume == 0], inplace=True)
-    add_mean_std(report)
-    report['diff'] = report['volume'] - report['mean']
-    # Remove 'volume' for the validation set.
-    train, valid, test = split_data(report, valid_year=2018, valid_month=0)
-    yv = valid['diff'].to_numpy()
-    valid['diff'] = pd.NA
-    report = pd.concat([train, valid])
+    # Split here to make the validation set more realistic.
+    report, test = split_data(report)
 
     report = pd.get_dummies(report, columns=['year', 'month'])
     report[['lactation', 'age']] = StandardScaler().fit_transform(report[['lactation', 'age']])
-    add_mean_std(report)
 
-    train, valid = split_data(report, col='diff')
+    train, valid = train_test_split(report, test_size=0.13, random_state=1)
+    logging.warning(f' Number of test data: {test.shape[0]}')
+    logging.warning(f' Number of validation data: {valid.shape[0]}')
 
-    drop_list = ['serial', 'volume']
-    train.drop(drop_list, axis=1, inplace=True)
-    valid.drop(drop_list, axis=1, inplace=True)
-
-    x = train.drop('diff', axis=1)
-    xv = valid.drop('diff', axis=1)
-    y = train['diff'].to_numpy()
+    x = train.drop(columns=['volume'])
+    xv = valid.drop(columns=['volume'])
+    y = train['volume'].to_numpy()
+    yv = valid['volume'].to_numpy()
 
     regr = RandomForestRegressor()
     regr.fit(x, y)
     yp = regr.predict(xv)
     print('RMSE =', RMSE(yv, yp))
-    print(regr.feature_names_in_)
-    print(regr.feature_importances_)
+    importances = pd.DataFrame(columns=['feature_names', 'importances'])
+    importances['feature_names'] = regr.feature_names_in_
+    importances['importances'] = regr.feature_importances_
+    print(importances)
     plt.scatter(yp, yv, label='SVR')
     plt.axis([0, 60, 0, 60])
     plt.xlabel('Predict')
@@ -68,11 +64,13 @@ def get_all_features() -> pd.DataFrame:
     df.drop(df.index[df.ID == 6960], inplace=True)
     df.drop(df.index[df.ID == 16714], inplace=True)
     df.drop(df.index[df.age < 21], inplace=True)
+    # Virgin Mary
+    # df.breeding = np.where(df.breeding == 0, df.delivery, df.breeding)
     # Frequency encoding
+    df.father.fillna(value=-1, inplace=True)
+    df.mother.fillna(value=-1, inplace=True)
     df.father = df.father.map(df.father.value_counts().to_dict())
     df.mother = df.mother.map(df.mother.value_counts().to_dict())
-    df.father.fillna(value=df.father.mean(), inplace=True)
-    df.mother.fillna(value=df.mother.mean(), inplace=True)
     df['serial_freq'] = df.serial.map(df.serial.value_counts().to_dict())
     # One hot encoding
     df = pd.get_dummies(df, columns=['ranch'])
@@ -84,7 +82,7 @@ def get_all_features() -> pd.DataFrame:
     df.drop(columns=['ID', 'birthday'], inplace=True)
     # Use integer.
     df.lactation = df.lactation.astype('int64')
-    logging.info(f'Number of unique\n{df.dtypes}')
+    logging.info(f' Number of unique\n{df.nunique()}')
     return df
 
 
