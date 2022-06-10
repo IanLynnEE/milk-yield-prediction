609ad403b3c1e3691484ca496d13580bf426d598
IanLynnEE <66598373+IanLynnEE@users.noreply.github.com>
2022-06-10 18:14:04 +0800

    Update preprocessing.py 1. Simplify add_mean_std() 2. Make space for other encoding

--- preprocessing.py
+++ preprocessing.py
@@ -10,18 +10,11 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.svm import SVR
 
 def main():
-    train, test = get_all_features()
-    # Remove 64 data with volume == 0. 
-    train.drop(train.index[train.volume == 0], inplace=True)
-    # Realistic validation set based on number of new cows
-    valid = train.query('year == 2018 & month > 9').copy()
-    train.query('year < 2018 | month <= 9', inplace=True)
-    # Just like in real case, we don't know all the mean and std.
-    add_mean_std(train, valid)
-    # If we have mean and std, drop other features
-    drop_list = ['ID', 'ranch', 'serial', 'father', 'mother']
-    # Play with other features
-    drop_list.extend(['year', 'month'])
+    report = get_all_features()
+    add_mean_std(report)
+    train, valid, test = split_data(report)
+
+    drop_list = ['serial', 'father', 'mother', 'year', 'month']
     train.drop(drop_list, axis=1, inplace=True)
     valid.drop(drop_list, axis=1, inplace=True)
 
@@ -34,11 +27,11 @@ def main():
     regr.fit(x, y)
     yp = regr.predict(xv)
     print('RMSE =', RMSE(yv, yp))
-    plt.scatter(yv, yp, label='SVR')
-    plt.scatter(yv, valid['mean'], label='Mean')
+    plt.scatter(yp, yv, label='SVR')
+    plt.scatter(valid['mean'], yv, label='Mean')
     plt.axis([0, 60, 0, 60])
-    plt.xlabel('True')
-    plt.ylabel('Predict')
+    plt.xlabel('Predict')
+    plt.ylabel('Truth')
     plt.legend()
     plt.show()
     return
@@ -48,7 +41,7 @@ def RMSE(yt, yp):
     return np.sum((yt - yp) ** 2) / yp.shape[0]
 
 
-def get_all_features() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+def get_all_features() -> pd.DataFrame:
     report = pd.read_csv('data/report.csv', skiprows=1,
                 names=[
                     'ID', 'year', 'month', 'ranch',
@@ -64,7 +57,7 @@ def get_all_features() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     # B. Simple encoding
     report.father = LabelEncoder().fit_transform(report.father)
     report.mother = LabelEncoder().fit_transform(report.mother)
-    report.ranch = LabelEncoder().fit_transform(report.ranch)
+    report = pd.get_dummies(report, columns=['ranch'])
     # C. Encode serial based on birthday.
     unique_serial = report.sort_values(by='birthday').serial.unique()
     new_serial_map = {old: i for i, old in enumerate(unique_serial)}
@@ -73,9 +66,13 @@ def get_all_features() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     report.drop(columns=['ID', 'birthday'], inplace=True)
     # After dropping data with NaN, use integer.
     report.lactation = report.lactation.astype('int64')
+    return report
+
+
+def split_data(df: pd.DataFrame) -> tuple:
     # Get training and test set.
-    train = pd.DataFrame.copy(report.loc[report['volume'].notnull()])
-    test  = pd.DataFrame.copy(report.loc[report['volume'].isnull()])
+    train = pd.DataFrame.copy(df.loc[df['volume'].notnull()])
+    test  = pd.DataFrame.copy(df.loc[df['volume'].isnull()])
     # Remove 64 data with volume == 0.
     train.drop(train.index[train.volume == 0], inplace=True)
     # A realistic validation set based on number of new cows
@@ -86,34 +83,21 @@ def get_all_features() -> tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     return train, valid, test
 
 
-def add_mean_std(train: pd.DataFrame, test: pd.DataFrame) -> pd.DataFrame:
-    """Add the average volume of each cow at each deliveray.
+def add_mean_std(df: pd.DataFrame):
     """
-    for i in train['serial'].unique():
-        for j in train.loc[train.serial == i, 'delivery'].unique():
-            index = train.index[(train.serial == i) & (train.delivery == j)]
-            train.loc[index, 'mean'] = train.loc[index, 'volume'].mean()
-            train.loc[index, 'std'] = train.loc[index, 'volume'].std()
-    train['std'].fillna(value=0, inplace=True)
-    # For cows in the test set, we try to find the same cow from training set.
-    # If the same cow can be found, yet it got new delivery, 
-    # we should fill the mean/std from last delivery from itself.
-    for i in test['serial'].unique():
-        for j in sorted(test.loc[test.serial == i, 'delivery'].unique()):
-            same_delivery = train.query(f'serial == {i} & delivery == {j}')
-            if same_delivery.shape[0] == 0:
-                same_delivery = train.query(f'serial=={i} & delivery=={j-1}')
-            index = test.index[(test.serial == i) & (test.delivery == j)]
-            test.loc[index, 'mean'] = same_delivery['volume'].mean()
-            test.loc[index, 'std'] = same_delivery['volume'].std()
-    logging.warning(
-        " Number of new cows in test: " +
-        f"{test.drop_duplicates(subset='serial')['mean'].isnull().sum()}"
-    )
-    valid_old_cows = test.dropna().copy()
-    test['mean'].fillna(value=train['volume'].mean(), inplace=True)
-    test['std'].fillna(value=0, inplace=True)
-    return valid_old_cows
+    Add mean and std of each cow at each deliveray.
+    Kind of like TargetEncoder, but it uses two columns to encode.
+    Moreover, it gives std as well.
+    """
+    for i in df['serial'].unique():
+        for j in df.loc[df.serial == i, 'delivery'].unique():
+            index = df.index[(df.serial == i) & (df.delivery == j)]
+            df.loc[index, 'mean'] = df.loc[index, 'volume'].mean()
+            df.loc[index, 'std'] = df.loc[index, 'volume'].std()
+    # TODO What should we fill for new cows and cows with new delivery?
+    df['mean'].fillna(value=0, inplace=True)
+    df['std'].fillna(value=0, inplace=True)
+    return
 
 
 if __name__ == '__main__':
