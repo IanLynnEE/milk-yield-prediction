61f27ce2bd7a0a3f7f2b16fc7575b8862ac0fe09
IanLynnEE <66598373+IanLynnEE@users.noreply.github.com>
2022-06-11 00:39:50 +0800

    Update preprocessing.py 1. Simplify split_data() 2. Add frequency encoding to get_all_features()

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,4 @@
-import logging 
+import logging
 # logging.basicConfig(level=logging.INFO)
 
 import pandas as pd
@@ -11,21 +11,25 @@ from sklearn.svm import SVR
 
 def main():
     report = get_all_features()
+    # Remove 64 training data with volume == 0.
+    report.drop(report.index[report.volume == 0], inplace=True)
+    # Remove 'volume' for the validation set.
     train, valid, test = split_data(report, valid_year=2018, valid_month=10)
-
     yv = valid['volume'].to_numpy()
     valid['volume'] = pd.NA
     report = pd.concat([train, valid])
+
+    report = pd.get_dummies(report, columns=['year', 'month'])
     add_mean_std(report)
 
-    train, _, valid = split_data(report, valid_year=2018, valid_month=10)
+    train, valid = split_data(report)
 
-    drop_list = ['serial', 'father', 'mother', 'year', 'month']
+    drop_list = ['serial']
     train.drop(drop_list, axis=1, inplace=True)
     valid.drop(drop_list, axis=1, inplace=True)
 
-    x = train.drop('volume', axis=1).to_numpy()
-    xv = valid.drop('volume', axis=1).to_numpy()
+    x = train.drop('volume', axis=1)
+    xv = valid.drop('volume', axis=1)
     y = train['volume'].to_numpy()
 
     regr = SVR()
@@ -48,7 +52,7 @@ def RMSE(yt, yp):
 
 
 def get_all_features() -> pd.DataFrame:
-    report = pd.read_csv('data/report.csv', skiprows=1,
+    df = pd.read_csv('data/report.csv', skiprows=1,
                 names=[
                     'ID', 'year', 'month', 'ranch',
                     'serial', 'father','mother', 'birthday',
@@ -56,35 +60,39 @@ def get_all_features() -> pd.DataFrame:
                 usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13],
                 dtype={'father': str, 'mother': str},
                 parse_dates=['birthday'])
-    # A. From the result of verifing data.
-    report.drop(report.index[report['ID'] == 6960], inplace=True)
-    report.drop(report.index[report['ID'] == 16714], inplace=True)
-    report.drop(report.index[report['age'] < 21], inplace=True)
-    # B. Simple encoding
-    report.father = LabelEncoder().fit_transform(report.father)
-    report.mother = LabelEncoder().fit_transform(report.mother)
-    report = pd.get_dummies(report, columns=['ranch'])
-    # C. Encode serial based on birthday.
-    unique_serial = report.sort_values(by='birthday').serial.unique()
-    new_serial_map = {old: i for i, old in enumerate(unique_serial)}
-    report.serial = report.serial.map(new_serial_map)
-    # Don't need birthday and ID now.
-    report.drop(columns=['ID', 'birthday'], inplace=True)
-    # After dropping data with NaN, use integer.
-    report.lactation = report.lactation.astype('int64')
-    return report
+    # Drop some data based on the result of verifing data.
+    df.drop(df.index[df.ID == 6960], inplace=True)
+    df.drop(df.index[df.ID == 16714], inplace=True)
+    df.drop(df.index[df.age < 21], inplace=True)
+    # Frequency encoding
+    df.father.fillna(value=-1, inplace=True)
+    df.father = df.father.map(df.father.value_counts().to_dict())
+    df.mother.fillna(value=-1, inplace=True)
+    df.mother = df.mother.map(df.mother.value_counts().to_dict())
+    df['serial_freq'] = df.serial.map(df.serial.value_counts().to_dict())
+    # One hot encoding
+    df = pd.get_dummies(df, columns=['ranch'])
+    # Ascending serial based on birthday.
+    unique_serial = df.sort_values(by='birthday').serial.unique()
+    serial_map = {old: i for i, old in enumerate(unique_serial)}
+    df.serial = df.serial.map(serial_map)
+    # Drop birthday and ID now.
+    df.drop(columns=['ID', 'birthday'], inplace=True)
+    # Use integer.
+    df.lactation = df.lactation.astype('int64')
+    logging.info(f'Number of unique\n{df.dtypes}')
+    return df
 
 
-def split_data(df: pd.DataFrame, valid_year: int, valid_month: int) -> tuple:
+def split_data(df: pd.DataFrame, valid_year=0, valid_month=0) -> tuple:
     # Get training and test set.
     train = pd.DataFrame.copy(df.loc[df['volume'].notnull()])
     test  = pd.DataFrame.copy(df.loc[df['volume'].isnull()])
-    # Remove 64 data with volume == 0.
-    train.drop(train.index[train.volume == 0], inplace=True)
+    if valid_year == 0:
+        return train, test
     # A realistic validation set based on number of new cows
     valid = train.query(f'year == {valid_year} & month > {valid_month}').copy()
     train.query(f'year < {valid_year} | month <= {valid_month}', inplace=True)
-    logging.info(f' Number of unique in train:\n{train.nunique()}')
     return train, valid, test
 
 
