4bc0dc6142024c2483291feb5c3aa10640db7dae
IanLynnEE <66598373+IanLynnEE@users.noreply.github.com>
2022-06-14 22:01:28 +0800

    Update main.py 1. Make it more easy to use. 2. Can merge two result if some test data are dropped

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from sklearn.neural_network import MLPRegressor
 from sklearn.metrics import mean_squared_error
 
 from preprocessing import read_report, drop_data, label_encoding
-from preprocessing import frequency_encoding, add_time_different, add_mean_std
+from preprocessing import frequency_encoding
+from preprocessing import add_delivery_season
+from preprocessing import add_time_different, add_mean_std
 
 
 def main():
@@ -17,17 +19,32 @@ def main():
     report = read_report()
     drop_data(report)
     label_encoding(report)
-    # frequency_encoding(report)
-    # one hot encoding
-    # report = pd.get_dummies(report, columns=['ranch'])
+    frequency_encoding(report)
+    add_delivery_season(report)
 
-    drop_cols = [
-        'ID',
-        'birthday', 'deliveryDate','samplingDate', 'recordDate',
-        'lastBreedDate', 'lastDeliveryDate', 'firstBreedDate',
-        'lastSemen', 'firstSemen'
+    # add_time_different(report)
+    # report.dropna(subset='intervalA', inplace=True)
+
+    # add_mean_std(report)
+    # report.dropna(subset='std', inplace=True)
+
+    use_cols = [
+        'year',
+        'month',
+        'ranch',
+        'serial',
+        'father', 'mother',
+        # 'firstSemen', 'lastSemen',
+        'delivery', 'lactation', 'age',
+        'breeding',
+        # 'deliverySeason',
+        # 'intervalA', 'intervalB', 'intervalC', 'intervalD', 'intervalE',
+        # 'mean', 'std',
+        'volume'
     ]
-    report.drop(columns=drop_cols, inplace=True)
+    data = report[use_cols]
+    data = pd.get_dummies(data, columns=['ranch'])
+    data.info()
 
     # ==================== Select Scaler Here ====================== #
     features_scaler = MinMaxScaler()
@@ -41,30 +58,33 @@ def main():
     )
 
     test  = report.loc[report['volume'].isnull()]
-    train = report.loc[report['volume'].notnull()]
+    train = data.loc[data['volume'].notnull()]
     logging.info(f' Number of test data: {test.shape[0]}')
 
-    check_model(train, features_scaler, regr)
+    for _ in range(10):
+        check_model(train, features_scaler, regr, time_valid=False)
 
-    yp = predict_test(report, features_scaler, regr)
+    yp = predict_test(data, features_scaler, regr)
     write_answer(yp)
+    ID = test['ID'].to_numpy()
+    np.save('ID1.npy', ID)
+    np.save('answer1.npy', yp)
     return
 
 
-def check_model(train: pd.DataFrame, features_scaler, regr):
+def check_model(train: pd.DataFrame, features_scaler, regr, time_valid=True):
     Y = train['volume']
     X = train.drop(columns='volume')
     X[:] = features_scaler.fit_transform(X)
 
-    # Special validation set
-    # index = train.index[(train.year == 2018) & (train.month >= 7)]
-    # xv = X.loc[index].to_numpy()
-    # yv = Y.loc[index].to_numpy()
-    # x = X.drop(index).to_numpy()
-    # y = Y.drop(index).to_numpy()
-
-    # Random validation set
-    x, xv, y, yv = train_test_split(X, Y, test_size=0.12, random_state=0)
+    if time_valid:
+        index = train.index[(train.year == 2018) & (train.month >= 7)]
+        xv = X.loc[index]
+        yv = Y.loc[index]
+        x = X.drop(index)
+        y = Y.drop(index)
+    else:
+        x, xv, y, yv = train_test_split(X, Y, test_size=0.12)
     
     logging.info(f' Number of validation data: {xv.shape[0]}')
     
@@ -93,6 +113,26 @@ def predict_test(report: pd.DataFrame, features_scaler, regr) -> np.ndarray:
     return regr.predict(xt_scaled)
 
 
+def merge_answer():
+    x1 = np.load('ID1.npy')
+    x2 = np.load('ID2.npy')
+    y1 = np.load('answer1.npy')
+    y2 = np.load('answer2.npy')
+    ID_list = pd.read_csv('./data/submission.csv')['ID'].to_numpy().tolist()
+    yp = np.zeros(len(ID_list))
+    for i, ID in enumerate(ID_list):
+        idx1 = np.where(x1 == ID)
+        idx2 = np.where(x2 == ID)
+        if len(idx1[0]) == 0:
+            if len(idx2) == 0:
+                logging.error(f'Cannot find ID = {ID}')
+            else:
+                yp[i] = y2[idx2[0][0]]
+        else:
+            yp[i] = y1[idx1[0][0]]
+    return yp
+
+
 def write_answer(yp: np.ndarray):
     report = read_report()
     test  = report.loc[report['volume'].isnull()]
@@ -101,10 +141,10 @@ def write_answer(yp: np.ndarray):
     template = pd.read_csv('./data/submission.csv')['ID'].to_numpy()
     for i, ID in enumerate(answer['ID'].to_numpy()):
         if template[i] != ID:
-            print('ERROR! Anser for ID =', ID)
+            logging.error(' Anser ID mismatched: ID =', ID)
     if answer.shape[0] != yp.shape[0]:
-        print('ERROR! Number of prediction incorrect!')
-        print('yp.shape[0]:', yp.shape[0])
+        logging.error(' Number of prediction incorrect!')
+        logging.error(f' yp.shape[0] = {yp.shape[0]}')
         return
     answer.volume = yp
     answer.to_csv('submission.csv', index=False)
@@ -113,3 +153,6 @@ def write_answer(yp: np.ndarray):
 
 if __name__ == '__main__':
     main()
+
+    # yp = merge_answer()
+    # write_answer(yp)
